6cef8ac84e611ce6255dac79589dfe439e71e13c
Lord Turmoil <swift-phantom@outlook.com>
2025-01-08 10:38:00 +0800

    🐛fixed module duplication

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["src/index.ts"],
-    clean: true,
     format: ["cjs", "esm"],
+    external: ["qvog-engine"],
+    clean: true,
     dts: true,
 });
