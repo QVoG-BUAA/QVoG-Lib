1348921a6f0db117fd80b3f7816f18d4d72175a7
Lord Turmoil <swift-phantom@outlook.com>
2025-03-02 11:07:06 +0800

    ✨add a chainable predicate 🧪add unit testing 🏷️bump dependency versions to fix vulnerability

--- src/lib/predicate/Predicates.ts
+++ src/lib/predicate/Predicates.ts
@@ -1,22 +1,188 @@
-import { CodeNode, CodeNodePredicate, CodeNodePredicateFn, Configuration, Value, ValuePredicate } from "qvog-engine";
+/**
+ * A generic predicate type.
+ * 
+ * It means by giving a value of type T, it returns a boolean indicating
+ * whether the value satisfies the predicate.
+ * 
+ * @typeParam T The type of the value.
+ * 
+ * @category Predicate
+ */
+type Predicate<T> = (value: T) => boolean;
 
 /**
- * Adapt predicate for code node to value predicate.
+ * A generic field function type.
  * 
- * The predicates used in DSL API are mainly for `Value`, so this
- * function can be used to fit the code node predicate into the value predicate.
- * Simply wraps it around your code node predicate.
+ * It means by giving a value of type T, it returns field of value of type U.
  * 
- * @param predicate The predicate to test the code node.
- * @returns Converted value predicate.
+ * @typeParam T The type of the value.
+ * @typeParam U The type of the field.
  * 
- * @category Predicates
+ * @category Predicate
  */
-export function asCodeNode(predicate: CodeNodePredicate | CodeNodePredicateFn): ValuePredicate {
-    return ValuePredicate.of(
-        (value: Value) => {
-            const node = Configuration.getContext().getNode(value);
-            const func = (predicate instanceof CodeNodePredicate) ? predicate.test : predicate;
-            return (node instanceof CodeNode) && func(node);
-        });
+type FieldFn<T, U> = (value: T) => U;
+
+/**
+ * Initial chainable predicate object.
+ * 
+ * @typeParam U The type of the value.
+ * 
+ * @category Predicate
+ */
+export class _P<U> {
+    private predicate: Predicate<U>;
+
+    constructor(predicate: Predicate<U>) {
+        this.predicate = predicate;
+    }
+
+    /**
+     * Apply the predicate to a value.
+     * 
+     * @param value The value to test.
+     * @returns true if the value satisfies the predicate, otherwise false.
+     */
+    test(value: U): boolean {
+        return this.predicate(value);
+    }
+
+    /**
+     * Chain another predicate with AND operator.
+     * 
+     * If `field` is not provided, it will continue to use the current value
+     * as the subject of the predicate.
+     * 
+     * @typeParam V The type of the field.
+     * 
+     * @param predicate Predicate.
+     * @param field Field function.
+     * @returns Chainable predicate object.
+     */
+    and<V>(predicate: _P<V> | _Q<V, V> | Predicate<V>, field?: FieldFn<U, V>): _Q<U, V> {
+        if (!field) {
+            field = (value: U) => value as unknown as V;
+        }
+        const fn = ((predicate instanceof _P) || (predicate instanceof _Q))
+            ? (v: V) => predicate.test(v)
+            : predicate;
+        return new _Q<U, V>(
+            (value: U) => this.predicate(value) && fn(field(value)),
+            field
+        );
+    }
+
+    /**
+     * Chain another predicate with OR operator.
+     * 
+     * If `field` is not provided, it will continue to use the current value
+     * as the subject of the predicate.
+     * 
+     * @typeParam V The type of the field.
+     * 
+     * @param predicate Predicate.
+     * @param field Field function.
+     * @returns Chainable predicate object.
+     */
+    or<V>(predicate: _P<V> | _Q<V, V> | Predicate<V>, field?: FieldFn<U, V>): _Q<U, V> {
+        if (!field) {
+            field = (value: U) => value as unknown as V;
+        }
+        const fn = ((predicate instanceof _P) || (predicate instanceof _Q))
+            ? (v: V) => predicate.test(v)
+            : predicate;
+        return new _Q<U, V>(
+            (value: U) => this.predicate(value) || fn(field(value)),
+            field
+        );
+    }
+}
+
+/**
+ * A chainable predicate object.
+ * 
+ * It support a predicate chain from type U to type V.
+ * 
+ * @typeParam U The initial type of the value.
+ * @typeParam V The current type of the value.
+ * 
+ * @category Predicate
+ */
+export class _Q<U, V> {
+    private predicate: Predicate<U>;
+    private field: FieldFn<U, V>;
+
+    constructor(predicate: Predicate<U>, field: FieldFn<U, V>) {
+        this.predicate = predicate;
+        this.field = field;
+    }
+
+    /**
+     * Apply the predicate to a value.
+     * 
+     * @param value The value to test.
+     * @returns true if the value satisfies the predicate, otherwise false.
+     */
+    test(value: U): boolean {
+        return this.predicate(value);
+    }
+
+    /**
+     * Chain another predicate with AND operator.
+     * 
+     * If `field` is not provided, it will continue to use the current value
+     * as the subject of the predicate.
+     * 
+     * @typeParam W The type of the field.
+     * 
+     * @param predicate Predicate.
+     * @param field Field function.
+     * @returns Chainable predicate object.
+     */
+    and<W>(predicate: _P<W> | Predicate<W>, field?: FieldFn<V, W>): _Q<U, W> {
+        if (!field) {
+            field = (value: V) => value as unknown as W;
+        }
+        const fn = (predicate instanceof _P)
+            ? (v: W) => predicate.test(v)
+            : predicate;
+        return new _Q<U, W>(
+            (value: U) => this.predicate(value) && fn(field(this.field(value))),
+            (value: U) => field(this.field(value))
+        );
+    }
+
+    /**
+     * Chain another predicate with OR operator.
+     * 
+     * If `field` is not provided, it will continue to use the current value
+     * as the subject of the predicate.
+     * 
+     * @typeParam W The type of the field.
+     * 
+     * @param predicate Predicate.
+     * @param field Field function.
+     * @returns Chainable predicate object.
+     */
+    or<W>(predicate: _P<W> | Predicate<W>, field: FieldFn<V, W>): _Q<U, W> {
+        if (!field) {
+            field = (value: V) => value as unknown as W;
+        }
+        const fn = (predicate instanceof _P)
+            ? (v: W) => predicate.test(v)
+            : predicate;
+        return new _Q<U, W>(
+            (value: U) => this.predicate(value) || fn(field(this.field(value))),
+            (value: U) => field(this.field(value))
+        );
+    }
+}
+
+/**
+ * Function wrapper for chainable predicate object.
+ * 
+ * @param predicate The initial predicate.
+ * @returns A chainable predicate object.
+ */
+export function P<U>(predicate: Predicate<U>): _P<U> {
+    return new _P<U>(predicate);
 }
---
+++ test/lib/Predicate.test.ts
@@ -0,0 +1,50 @@
+import { expect, test } from "vitest";
+
+import { P } from "../../src/lib/predicate";
+
+class A {
+    public b: B;
+    public value: number;
+
+    constructor(x: number, y: number, z: number) {
+        this.value = x;
+        this.b = new B(y, z);
+    }
+}
+
+class B {
+    public c: C;
+    public value: number;
+
+    constructor(y: number, z: number) {
+        this.value = y;
+        this.c = new C(z);
+    }
+}
+
+class C {
+    public value: number = 0;
+
+    constructor(z: number) {
+        this.value = z;
+    }
+}
+
+test("Predicate test", () => {
+    const a1 = new A(1, 2, 3);
+    const a2 = new A(4, 5, 6);
+    const a3 = new A(-7, 8, 9);
+
+    const predicate = P<A>(a => a.value > 0)
+        .and<B>(
+            P<B>(b => b.value < 0).or(b => b.value > 3),
+            a => a.b
+        ).and<C>(
+            c => c.value > 0,
+            b => b.c
+        );
+
+    expect(predicate.test(a1)).toBe(false);
+    expect(predicate.test(a2)).toBe(true);
+    expect(predicate.test(a3)).toBe(true);
+});
